3bc821b3d4c8c29f4a4518b6865be7b44a89ff95
Robin Löffel <hi@robinloeffel.ch>
2025-02-07 19:09:44 +0100

    refactor: drastically simplify lightbox and gallery

--- app/src/lib/components/atoms/lightbox.svelte
+++ app/src/lib/components/atoms/lightbox.svelte
@@ -1,120 +1,64 @@
 <script lang="ts">
   import { Icon } from "$components";
 
-  export let index = -1;
-  export let itemsCount = 0;
+  interface LightboxImage {
+    src: string;
+    alt: string;
+  }
 
-  let dialog: HTMLDialogElement;
-  let open = false;
+  interface Props {
+    image: LightboxImage;
+    close: () => void;
+  }
 
-  export const close = () => {
-    document.body.classList.remove("no-scroll");
-    dialog.close();
-    open = false;
-  };
+  const { image, close }: Props = $props();
+  let visible = $state(false);
 
-  export const show = () => {
-    document.body.classList.add("no-scroll");
-    dialog.showModal();
-    open = true;
-  };
-
-  const previous = () => {
-    index = index === 0 ? itemsCount - 1 : index - 1;
-  };
-
-  const next = () => {
-    index = index === itemsCount - 1 ? 0 : index + 1;
-  };
-
-  const handleWindowKeydown = ({ key }: KeyboardEvent) => {
-    if (open) {
-      if (key === "ArrowLeft") {
-        previous();
-      }
-
-      if (key === "ArrowRight") {
-        next();
-      }
-    }
+  const show = () => {
+    visible = true;
   };
 </script>
 
-<svelte:window on:keydown={handleWindowKeydown} />
-
-<dialog bind:this={dialog} style:--item={index} class="lightbox" title="Diashow">
-  <button class="lightbox-button" type="button" on:click={close}>
+<div class="lightbox" class:visible>
+  <button onclick={close} type="button">
     <Icon name="x-mark" size="medium" />
-    <span class="sr-only">Diashow schliessen</span>
+    <span class="sr-only">Schliessen</span>
   </button>
-  <ul class="lightbox-track">
-    <slot></slot>
-  </ul>
-  <div class="lightbox-navigation">
-    <button class="lightbox-button" type="button" on:click={previous}>
-      <Icon name="arrow-left" size="medium" />
-      <span class="sr-only">Zurück blättern</span>
-    </button>
-    <button class="lightbox-button" type="button" on:click={next}>
-      <Icon name="arrow-right" size="medium" />
-      <span class="sr-only">Weiter blättern</span>
-    </button>
-  </div>
-</dialog>
+  <img class="image" alt={image.alt} onload={show} src={image.src} />
+</div>
 
 <style lang="scss">
-  @use "$styles/scales";
   @use "$styles/colors";
-  @use "$styles/breakpoints";
-
-  :global(::backdrop) {
-    background: none;
-    backdrop-filter: blur(scales.space("64")) brightness(0.2);
-  }
+  @use "$styles/scales";
 
   .lightbox {
-    grid-template-rows: auto 1fr auto;
-    place-items: center;
-    width: 100%;
-    max-width: none;
-    height: 100%;
-    max-height: none;
-    overflow: hidden;
-    background: none;
-    border: 0;
-
-    &[open] {
-      display: grid;
-    }
-  }
-
-  .lightbox-track {
+    position: fixed;
+    inset: 0;
+    z-index: 10;
     display: grid;
-    grid-auto-columns: 100%;
-    grid-auto-flow: column;
-    height: 100%;
-    min-height: 0;
-    list-style: none;
-    translate: calc(var(--item) * -100%);
+    place-items: center;
+    background-color: color-mix(in srgb, colors.$charcoal 95%, transparent);
+    backdrop-filter: blur(scales.space("32"));
+    opacity: 0;
+    transition: opacity 0.5s;
 
-    :global(> *) {
-      display: grid;
-      place-items: center;
-      min-height: 0;
-      padding: scales.space("24");
+    &.visible {
+      opacity: 1;
     }
   }
 
-  .lightbox-button {
-    display: inline-grid;
-    padding: scales.space("16");
+  img {
+    inset: 0;
+    padding: scales.space("32");
+  }
+
+  button {
+    position: absolute;
+    inset: scales.space("32") scales.space("32") auto auto;
+    display: grid;
     color: colors.$white;
     cursor: pointer;
     background: 0;
     border: 0;
-  }
-
-  .lightbox-navigation {
-    display: flex;
   }
 </style>
--- app/src/lib/components/blocks/image-gallery.svelte
+++ app/src/lib/components/blocks/image-gallery.svelte
@@ -1,6 +1,9 @@
 <script lang="ts">
   import { urlFor, type Result } from "$cms";
   import { Grid, Image, Lightbox } from "$components";
+  import type { ComponentProps } from "svelte";
+
+  type LightboxImage = ComponentProps<typeof Lightbox>["image"];
 
   interface Props {
     images: Result<"imageGallery", "images">;
@@ -9,56 +12,68 @@
   const { images }: Props = $props();
 
   const uiImages = $derived(
-    images.map(item => ({
-      _key: item._key,
-      alt: item.alt,
-      thumbnail: urlFor(item)
+    images.map(record => ({
+      _key: record._key,
+      alt: record.alt,
+      thumb: urlFor(record)
         .auto("format")
         .size(600, 600)
         .url(),
-      full: urlFor(item)
+      full: urlFor(record)
         .auto("format")
         .width(1920)
         .url()
     }))
   );
 
-  let show: VoidFunction;
-  let openLightboxElementIndex = -1;
+  let lightboxImage = $state<LightboxImage | null>(null);
+  let bodyRef: HTMLElement;
 
-  const openLightbox = (index: number) => {
-    show();
-    openLightboxElementIndex = index;
+  $effect(() => {
+    bodyRef.classList.toggle("no-scroll", Boolean(lightboxImage));
+  });
+
+  const setLightbox = (image: LightboxImage) => {
+    lightboxImage = image;
+  };
+
+  const closeLightbox = () => {
+    lightboxImage = null;
+  };
+
+  const handleWindowKeydown = ({ key }: KeyboardEvent) => {
+    if (lightboxImage && key === "Escape") {
+      closeLightbox();
+    }
   };
 </script>
 
+<svelte:window onkeydown={handleWindowKeydown} />
+<svelte:body bind:this={bodyRef} />
+
 <Grid tag="section">
   <ul class="image-gallery">
-    {#each uiImages as galleryImage, index (galleryImage._key)}
+    {#each uiImages as image (image._key)}
       <li class="image-gallery-item">
         <button
-          class="image-gallery-thumb"
-          onclick={() => { openLightbox(index); }}
+          class="image-gallery-button" onclick={() => {
+            setLightbox({ src: image.full, alt: image.alt });
+          }}
           type="button"
         >
-          <Image alt={galleryImage.alt} src={galleryImage.thumbnail} />
+          <Image alt={image.alt} src={image.thumb} />
         </button>
       </li>
     {/each}
   </ul>
 </Grid>
 
-<Lightbox
-  bind:index={openLightboxElementIndex}
-  bind:itemsCount={uiImages.length}
-  bind:show
->
-  {#each uiImages as lightboxImage (lightboxImage._key)}
-    <li>
-      <Image alt={lightboxImage.alt} src={lightboxImage.full} />
-    </li>
-  {/each}
-</Lightbox>
+{#if lightboxImage}
+  <Lightbox
+    close={closeLightbox}
+    image={lightboxImage}
+  />
+{/if}
 
 <style lang="scss">
   @use "$styles/scales";
@@ -83,7 +98,7 @@
     }
   }
 
-  .image-gallery-thumb {
+  .image-gallery-button {
     display: grid;
     place-items: center;
     overflow: hidden;
